c65135c65c15cea3c3c4ec5b245706a07693a507
karanwadhwa <wadhwa.karan96@gmail.com>
2018-03-15 17:22:58 +0530

    display retrieved videos

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const VideoListItem = (props) => {
   console.log(props.video);
+  const ImgURL = props.video.snippet.thumbnails.default.url;
+  const Title = props.video.snippet.title
   return (
-    <li> Video </li>
+    <li className='list-group-item'>
+      <div className='media'>
+        <img class="mr-3" src={ImgURL} alt="Generic placeholder image" />
+        <div class="media-body">
+          <h5 class="mt-0 mb-1">{Title}</h5>
+
+        </div>
+      </div>
+    </li>
   );
 };
 
