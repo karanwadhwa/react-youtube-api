6c05927e471541b0cb4dbdf31fbaed2ce8af4a7c
karanwadhwa <wadhwa.karan96@gmail.com>
2018-03-15 15:55:30 +0530

    retireve videos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/SearchBar';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+// intentionally left in
+//#idgaf #yolo
+const API_KEY = 'AIzaSyAkxiCZn_DaswfFUnBNgF3kK6bwyI3pM8w';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'cats'}, videos => this.setState({ videos }) );
+  }
+
   render() {
     return (
       <div className="App">
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/VideoListItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const VideoListItem = (props) => {
+  console.log(props.video);
+  return (
+    <li> Video </li>
+  );
+};
+
+export default VideoListItem;
